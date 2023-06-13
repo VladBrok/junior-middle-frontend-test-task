59d8413aa87c70775cd7feaade4296710a99baf1
VladBrok <vlad.brok99@gmail.com>
2023-06-13 17:32:17 +0300

    add ru locale

---
+++ src/lang/entries/ru_RU.js
@@ -0,0 +1,12 @@
+import antdRu from "antd/es/locale/ru_RU";
+import ruMsg from "../locales/ru_RU.json";
+
+const RuLang = {
+  antd: antdRu,
+  locale: "ru-RU",
+  messages: {
+    ...ruMsg,
+  },
+};
+
+export default RuLang;
