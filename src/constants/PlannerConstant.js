4c94c89eec8d717bc5f7e2f5e3b315985a63bb22
VladBrok <vlad.brok99@gmail.com>
2023-06-14 19:17:51 +0300

    cleanup

--- src/constants/PlannerConstant.js
+++ src/constants/PlannerConstant.js
@@ -1,5 +1,1 @@
-// TODO: first 2 not needed ?
-export const AVAILABLE_TABLES_DROPPABLE_ID = "available-tables-droppable";
-export const BOARD_DROPPABLE_ID = "board-droppable";
-
 export const TABLE_DRAG_ITEM = "table-drag-item";
--- src/views/app-views/main/planner/index.js
+++ src/views/app-views/main/planner/index.js
@@ -1,11 +1,5 @@
 import plannerTables from "assets/data/planner-tables.data.json";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import {
-  AVAILABLE_TABLES_DROPPABLE_ID,
-  BOARD_DROPPABLE_ID,
-  TABLE_DRAG_ITEM,
-} from "constants/PlannerConstant";
-import { List } from "antd";
+import { TABLE_DRAG_ITEM } from "constants/PlannerConstant";
 import "./index.css";
 import { useRef, useState } from "react";
 import Utils from "utils";
@@ -35,19 +29,19 @@ function PlannerTable({ id, index, description, image, move }) {
       }
 
       const hoverBoundingRect = ref.current?.getBoundingClientRect();
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      const hoverMiddleX =
+        (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
       const clientOffset = monitor.getClientOffset();
-      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return;
-      }
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+      const isDraggingLeftwards = dragIndex > hoverIndex;
+      const isDraggingRightwards = dragIndex < hoverIndex;
+      const isCrossedHalfLeftwards = hoverClientX < hoverMiddleX;
+      const isCrossedHalfRightwards = hoverClientX > hoverMiddleX;
+
+      if (
+        (isDraggingLeftwards && !isCrossedHalfLeftwards) ||
+        (isDraggingRightwards && !isCrossedHalfRightwards)
+      ) {
         return;
       }
 
@@ -82,56 +76,7 @@ function PlannerTable({ id, index, description, image, move }) {
   );
 }
 
-// TODO: remove commented code
-
 const Planner = () => {
-  // const [availableTables, setAvailableTables] = useState(plannerTables);
-  // const [boardTables, setBoardTables] = useState([]);
-  // const tableRefs = useRef([]);
-  // const boardRef = useRef();
-
-  // const handleDragEnd = (result) => {
-  //   console.log(result);
-
-  //   const isDroppableSame =
-  //     result.destination?.droppableId === result.source.droppableId;
-  //   const isDroppedToAvailableTables =
-  //     result.destination?.droppableId === AVAILABLE_TABLES_DROPPABLE_ID;
-  //   const isDroppedToBoard = Utils.intersect(
-  //     boardRef.current,
-  //     tableRefs.current[
-  //       (result.source.droppableId === AVAILABLE_TABLES_DROPPABLE_ID
-  //         ? availableTables[result.source.index]
-  //         : boardTables[result.source.index]
-  //       ).key
-  //     ]
-  //   );
-
-  //   // TODO: refactor: extract function for each condition
-  //   if (isDroppableSame && isDroppedToAvailableTables) {
-  //     const reordered = Utils.moveItem(
-  //       availableTables,
-  //       result.source.index,
-  //       result.destination.index
-  //     );
-  //     setAvailableTables(reordered);
-  //   } else if (!isDroppableSame && isDroppedToBoard) {
-  //     const table = availableTables[result.source.index];
-
-  //     const el = tableRefs.current[table.key];
-  //     console.log(el);
-
-  //     setBoardTables((prev) => [...prev, { ...table, top: 15, left: 20 }]);
-  //     setAvailableTables((prev) => prev.filter((x) => x !== table));
-  //   } else if (!isDroppableSame && isDroppedToAvailableTables) {
-  //     const table = boardTables[result.source.index];
-  //     const available = availableTables.slice();
-  //     available.splice(result.destination.index, 0, table);
-  //     setAvailableTables(available);
-  //     setBoardTables((prev) => prev.filter((x) => x !== table));
-  //   }
-  // };
-
   const [availableTables, setAvailableTables] = useState(plannerTables);
 
   const moveTable = (oldIdx, newIdx) => {
@@ -142,100 +87,19 @@ const Planner = () => {
   return (
     <>
       <DndProvider backend={HTML5Backend}>
-        {availableTables.map((table, index) => (
-          <PlannerTable
-            key={table.id}
-            id={table.id}
-            description={table.description}
-            image={table.image}
-            index={index}
-            move={moveTable}
-          />
-        ))}
+        <div className="planner__list-container">
+          {availableTables.map((table, index) => (
+            <PlannerTable
+              key={table.id}
+              id={table.id}
+              description={table.description}
+              image={table.image}
+              index={index}
+              move={moveTable}
+            />
+          ))}
+        </div>
       </DndProvider>
-
-      {/* <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable
-          droppableId={AVAILABLE_TABLES_DROPPABLE_ID}
-          direction="horizontal"
-        >
-          {(provided) => (
-            <div
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-              className="planner__available-tables"
-            >
-              <List
-                className="planner__list-container"
-                dataSource={availableTables}
-                renderItem={(table, index) => (
-                  <Draggable
-                    index={index}
-                    key={table.key}
-                    draggableId={table.key}
-                  >
-                    {(provided) => (
-                      <div
-                        ref={(el) => {
-                          tableRefs.current[table.key] = el;
-                          provided.innerRef(el);
-                        }}
-                        {...provided.dragHandleProps}
-                        {...provided.draggableProps}
-                        className="planner__item"
-                      >
-                        <div>{table.id}</div>
-                      </div>
-                    )}
-                  </Draggable>
-                )}
-              />
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-
-        <Droppable
-          droppableId={BOARD_DROPPABLE_ID}
-          direction="horizontal"
-          isDropDisabled={true}
-        >
-          {(provided) => (
-            <div
-              ref={(el) => {
-                boardRef.current = el;
-                provided.innerRef(el);
-              }}
-              {...provided.droppableProps}
-              className="planner__board"
-            >
-              {boardTables.map((table, index) => (
-                <Draggable
-                  index={index}
-                  key={table.key}
-                  draggableId={table.key}
-                >
-                  {(provided) => (
-                    <div
-                      ref={(el) => {
-                        tableRefs.current[table.key] = el;
-                        provided.innerRef(el);
-                      }}
-                      {...provided.dragHandleProps}
-                      {...provided.draggableProps}
-                      className="planner__item planner__board-item"
-                      style={{ top: table.top, left: table.left }}
-                    >
-                      <div>{table.id}</div>
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext> */}
     </>
   );
 };
